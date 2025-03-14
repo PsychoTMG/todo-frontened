f442850661e040494967d9c12c1077d287fee679
PsychoTMG <psychotmg@xiaomi-25-pro-ulta-250tb-5000mpx.local>
2025-03-14 12:11:23 +0300

    Добавлен метатег для Яндекс.Вебмастера

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ const Home = () => {
     <>
       <Head>
         <title>Мой ToDo List</title>
+        <meta name="yandex-verification" content="1c9b9ab69c3b700e" />
         <meta name="description" content="Управляй своими задачами легко!" />
         <meta name="keywords" content="todo, список дел, задачи, управление временем" />
         <meta name="robots" content="index, follow" />
