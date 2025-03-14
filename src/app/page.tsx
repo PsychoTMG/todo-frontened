0cb86b92d83bb05b20cb2b0be70214179d723127
PsychoTMG <psychotmg@xiaomi-25-pro-ulta-250tb-5000mpx.local>
2025-03-14 10:57:28 +0300

    Убраны не юзабельные переменные

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,15 @@
 "use client";
 
-import React, {  useState } from 'react';
 // import axios from 'axios';
 
-interface ApiResponce {
-  id: number,
-  title: string,
-  comleted: boolean
-}
+
 
 const Home = () => {
-  const [data, setData] = useState<ApiResponce[] | null>(null);
 
-  // useEffect(() => {
-  //   const fetch = async () => {
-  //     const responce = await axios.get<ApiResponce[]>('https://todo-backend-b3ts.onrender.com/todos');
-  //     setData(responce.data);
-  //   };
-
-  //   fetch();
-  // }, []);
 
   return (
     <div> Главная страница 
-      {/* {data ? data.map(item => <> <div>{item.id}</div><div>{item.title}</div></>) : null} */}
+    
     </div>
   );
 };
