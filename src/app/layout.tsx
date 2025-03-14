ae7d39a184140ce1d444fe49808912b9beab8be8
PsychoTMG <psychotmg@xiaomi-25-pro-ulta-250tb-5000mpx.local>
2025-03-14 13:03:21 +0300

    meta yandex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,48 +13,40 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <html lang="en">
-
-        <Head>
-          <meta name="yandex-verification" content="1c9b9ab69c3b700e" />
-        </Head>
-
+      <Head>
+        <meta name="yandex-verification" content="1c9b9ab69c3b700e" />
+      </Head>
       <body className="flex">
         {/* Боковая панель с анимацией */}
         <div
-          className={`fixed top-0 left-0 h-full bg-[#fffbf7] w-[280px] transition-transform duration-300 ${leftBar ? "translate-x-0" : "-translate-x-full"}`}>
+          className={`fixed top-0 left-0 h-full bg-[#fffbf7] w-[280px] transition-transform duration-300 ${leftBar ? "translate-x-0" : "-translate-x-full"}`}
+        >
           <LeftBar setOpenSearch={setOpenSearch} />
         </div>
 
         {/* Одна кнопка, которая смещается */}
         <button
           onClick={() => setLeftBar((prev) => !prev)}
-          className={`fixed top-7 left-5 text-white rounded transition-transform duration-300 ${leftBar ? "translate-x-[220px]" : "translate-x-0"
-            }`}
+          className={`fixed top-7 left-5 text-white rounded transition-transform duration-300 ${leftBar ? "translate-x-[220px]" : "translate-x-0"}`}
         >
-          {leftBar ? (
-            <div className={'cursor-pointer'}>
-              <Image src="/menu.svg" width={20} height={20} alt="Закрыть" />
-            </div>
-          ) : (
-            <div className={'cursor-pointer'}>
-              <Image src="/menu.svg" width={20} height={20} alt="Закрыть" />
-            </div>
-          )}
+          <div className={"cursor-pointer"}>
+            <Image src="/menu.svg" width={20} height={20} alt="Меню" />
+          </div>
         </button>
 
         {/* Основной контент */}
         <div
-          className={`flex w-full h-screen justify-center pt-10 transition-all duration-300 ${leftBar ? "ml-[280px]" : "ml-0"
-            }`}
+          className={`flex w-full h-screen justify-center pt-10 transition-all duration-300 ${leftBar ? "ml-[280px]" : "ml-0"}`}
         >
-          {openSearh &&
-            <div className='flex top-0 left-0 fixed justify-center items-center w-full h-screen z-50 bg-[#e1e1e123]'>
+          {openSearh && (
+            <div className="flex top-0 left-0 fixed justify-center items-center w-full h-screen z-50 bg-[#e1e1e123]">
               <Search />
-            </div>}
+            </div>
+          )}
           {children}
         </div>
       </body>
-    </html >
+    </html>
   );
 };
 
