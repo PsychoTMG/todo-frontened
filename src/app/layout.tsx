b70d647174a5ce994cc8e242b971a086fd6a697d
PsychoTMG <psychotmg@xiaomi-25-pro-ulta-250tb-5000mpx.local>
2025-03-14 12:29:51 +0300

    Добавлен метатег для Яндекс.Вебмастера

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 "use client";
+import Head from "next/head";
+
 import LeftBar from "@/components/LeftBar";
 import "./globals.css";
 import { useState } from "react";
@@ -12,7 +14,9 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <head>
-        <meta name="yandex-verification" content="1c9b9ab69c3b700e" />
+        <Head>
+          <meta name="yandex-verification" content="1c9b9ab69c3b700e" />
+        </Head>
       </head>
       <body className="flex">
         {/* Боковая панель с анимацией */}
