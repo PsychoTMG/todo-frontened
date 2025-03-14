73c3bdb66d817fbfdf1bbb564e7dd48191d96313
PsychoTMG <psychotmg@xiaomi-25-pro-ulta-250tb-5000mpx.local>
2025-03-14 13:05:15 +0300

    meta yandex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,9 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <html lang="en">
-      <Head>
+      <head>
         <meta name="yandex-verification" content="1c9b9ab69c3b700e" />
-      </Head>
+      </head>
       <body className="flex">
         {/* Боковая панель с анимацией */}
         <div
